2cfd60e851ab65966d1ae8f1207af09344fbcd9f
Vaneska <vaneskakaren15@gmail.com>
2020-10-18 23:24:58 -0300

    Organizando identação

--- src/main/java/com/totalcross/breakoutTC/util/Images.java
+++ src/main/java/com/totalcross/breakoutTC/util/Images.java
@@ -1,8 +1,8 @@
 package com.totalcross.breakoutTC.util;
 
-import totalcross.sys.Settings;
 import totalcross.ui.dialog.MessageBox;
 import totalcross.ui.image.Image;
+import static com.totalcross.breakoutTC.util.Constants.*;
 
 public class Images {
 
@@ -19,17 +19,12 @@ public class Images {
             title = new Image("others/breakout_title.png");
 
             // Bricks 20x32
-            red = new Image("sprites/red_brick.png").getScaledInstance(Constants.WIDTH_BRICKS, Constants.HEIGHT_BRICKS);
-            orange = new Image("sprites/orange_brick.png").getScaledInstance(Constants.WIDTH_BRICKS,
-                    Constants.HEIGHT_BRICKS);
-            dark_orange = new Image("sprites/orange2_brick.png").getScaledInstance(Constants.WIDTH_BRICKS,
-                    Constants.HEIGHT_BRICKS);
-            yellow = new Image("sprites/yellow_brick.png").getScaledInstance(Constants.WIDTH_BRICKS,
-                    Constants.HEIGHT_BRICKS);
-            green = new Image("sprites/green_brick.png").getScaledInstance(Constants.WIDTH_BRICKS,
-                    Constants.HEIGHT_BRICKS);
-            blue = new Image("sprites/blue_brick.png").getScaledInstance(Constants.WIDTH_BRICKS,
-                    Constants.HEIGHT_BRICKS);
+            red = new Image("sprites/red_brick.png").getScaledInstance(WIDTH_BRICKS, HEIGHT_BRICKS);
+            orange = new Image("sprites/orange_brick.png").getScaledInstance(WIDTH_BRICKS, HEIGHT_BRICKS);
+            dark_orange = new Image("sprites/orange2_brick.png").getScaledInstance(WIDTH_BRICKS, HEIGHT_BRICKS);
+            yellow = new Image("sprites/yellow_brick.png").getScaledInstance(WIDTH_BRICKS, HEIGHT_BRICKS);
+            green = new Image("sprites/green_brick.png").getScaledInstance(WIDTH_BRICKS, HEIGHT_BRICKS);
+            blue = new Image("sprites/blue_brick.png").getScaledInstance(WIDTH_BRICKS, HEIGHT_BRICKS);
 
         } catch (Exception e) {
             MessageBox.showException(e, true);
